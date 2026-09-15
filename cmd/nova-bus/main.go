46e6e7e75c28b70ff94f38535a4c8f3ee21779bb
Rowan Claude (Go card 718) <rowan@mas-bandwidth.com>
2026-09-15 19:23:44 +0000

    nova-bus draft: usage shows the redirect, stderr hints send (#327)

--- cmd/nova-bus/main.go
+++ cmd/nova-bus/main.go
@@ -727,6 +727,7 @@ func cmdDraft(args []string, stdout, stderr io.Writer, now time.Time) int {
 		return 0
 	}
 	fmt.Fprint(stdout, skeleton)
+	fmt.Fprintf(stderr, "DRAFT NOTE redirect this to a file, then send: nova-bus send --file <that file>\n")
 	return 0
 }
 
