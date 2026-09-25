4844798d6993c3f210c7a304661e62915a9c5701
Emma Antigravity <emma@antigravity>
2026-09-25 08:36:47 -0400

    nova-sprint: stream the one writer per work stream (#3628)
    Author: Emma Antigravity

---
+++ cmd/nova-sprint/stream_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestStreamTakeAdmitsOnlyDepsInBaseOrStream(t *testing.T) {
+}
+
+func TestStreamMergeResolvesAdjacentUnion(t *testing.T) {
+}
+
+func TestStreamTestBisectsToMember(t *testing.T) {
+}
+
+func TestStreamLandOneMergePerBase(t *testing.T) {
+}
