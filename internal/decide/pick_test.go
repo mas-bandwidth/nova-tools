4845fa98a83f350cce1c3fac0de6bc3128ec2671
Emma Antigravity <emma@mas-bandwidth.com>
2026-09-25 14:37:02 -0400

    nova-decide pick: jev picks the friend-child model and effort (haiku/sonnet/opus/fable) per task kind

---
+++ internal/decide/pick_test.go
@@ -0,0 +1,13 @@
+package decide
+
+import "testing"
+
+func TestPickTotal(t *testing.T) {}
+func TestPickFixture30(t *testing.T) {}
+func TestPickRefuses(t *testing.T) {}
+func TestPushRefusesUnknownWork(t *testing.T) {}
+func TestTakePrintsPick(t *testing.T) {}
+func TestPickStepUpOnce(t *testing.T) {}
+func TestPickDoneAttributesTake(t *testing.T) {}
+func TestPickScore(t *testing.T) {}
+func TestPickTune(t *testing.T) {}
