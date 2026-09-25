0e0fa2e0ba19e89d3cc33c6f1034df59be449aaa
Emma Antigravity <emma@mas-bandwidth.com>
2026-09-25 14:48:26 -0400

    nova-decide: fix gofmt in pick_test.go (#3223)

--- internal/decide/pick_test.go
+++ internal/decide/pick_test.go
@@ -2,12 +2,12 @@ package decide
 
 import "testing"
 
-func TestPickTotal(t *testing.T) {}
-func TestPickFixture30(t *testing.T) {}
-func TestPickRefuses(t *testing.T) {}
+func TestPickTotal(t *testing.T)              {}
+func TestPickFixture30(t *testing.T)          {}
+func TestPickRefuses(t *testing.T)            {}
 func TestPushRefusesUnknownWork(t *testing.T) {}
-func TestTakePrintsPick(t *testing.T) {}
-func TestPickStepUpOnce(t *testing.T) {}
+func TestTakePrintsPick(t *testing.T)         {}
+func TestPickStepUpOnce(t *testing.T)         {}
 func TestPickDoneAttributesTake(t *testing.T) {}
-func TestPickScore(t *testing.T) {}
-func TestPickTune(t *testing.T) {}
+func TestPickScore(t *testing.T)              {}
+func TestPickTune(t *testing.T)               {}
